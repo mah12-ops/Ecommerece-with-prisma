4a00dfe7042d1fc0b477f2725ea3f16fe19b5fc8
mercy <mihretyirga7@gmail.com>
2025-05-11 15:39:39 +0300

    updated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authRoutes from "./auth";
 import productsRoutes from "./products"
 import usersRoute from "./users";
+import cartRoutes from "./cart";
 
 
  const rootRouter:Router = Router()
@@ -9,4 +10,5 @@ import usersRoute from "./users";
 rootRouter.use("/auth" , authRoutes)
 rootRouter.use("/products" ,productsRoutes)
 rootRouter.use("/users" ,usersRoute)
+rootRouter.use("/carts" ,cartRoutes)
 export default rootRouter
